f51919622e73a9aa1558b864cc1161857ca56711
Diego Cardenas <dcardenas@unc.edu>
2022-01-23 13:53:00 -0500

    progress on ex_01

---
+++ exercises/ex_01_chardle.py
@@ -0,0 +1,7 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730427471"
+
+chosen_word: str = input("Enter a 5-character word ")
+chosen_letter: str = input("Enter a single character ")
+print("searching for " + chosen_letter + " in " + chosen_word)
