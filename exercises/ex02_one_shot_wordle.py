f20edba90fbf84836d196cbc82773c2294c4d250
Diego Cardenas <dcardenas@unc.edu>
2022-01-31 17:29:41 -0500

    completed ex 02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,17 +1,18 @@
 """Recreating wordle program with one chance to get word right."""
-___author___ = 730427471
+
+__author__ = "730427471"
 
 secret = "python"
 guess = input(f"What is your {len(secret)} letter guess? ")
-
-WHITE_BOX: str = " \U00002B1C"
-GREEN_BOX: str = " \U0001F7E9"
-YELLOW_BOX: str = " \U0001F7E8"
-
-index = 0
 boxes = ""
 match: bool = False 
+index = 0
 alt_index = 0
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
 while len(guess) != len(secret):   # if the length of the guess is not the length of the secret word then prompt them to enter another word
     guess = input(f"That was not {len(secret)} letters! Try again: ")
 
@@ -19,17 +20,18 @@ while index < len(secret):
     if guess[index] == secret[index]:  # if the corresponding indexes match, then the letter is in the correct place and a green box should be printed
         boxes = boxes + GREEN_BOX
     else: 
-        while match is False and alt_index < len(secret): 
+        while match is not True and alt_index < len(secret): 
             if guess[index] == secret[alt_index]:
-                boxes = boxes + YELLOW_BOX
-                match = True  # after a match is found and a yellow box is added to the boxes string, end the loop by declaring match true
+                match = True  # after a match is found, end the loop by declaring match true
             else:
-                alt_index = alt_index + 1  # increase alternate index by 1 to repeat the loop but check the next letter
-    if guess[index] != secret[index] and match is False:  # the first condition for a white box is that the corresponding indexes don't match, but also that there has been no match found in the whole word aka that the match bool has not been declared true and there is no yellow box added to the box string
-        boxes = boxes + WHITE_BOX
+                alt_index = alt_index + 1  # increase alternate index by 1 to repeat the loop but check the guess against the next letter of the secret
+        if match is True:  # if match has been declared true, then the letter did not match the corresponding index of the secret, but it matched one of the other indexes, so print a yellow box
+            boxes = boxes + YELLOW_BOX
+        else:
+            boxes = boxes + WHITE_BOX
     index = index + 1
-    match = False
-    alt_index = 0
+    match = False  # reset match to False, so if the letter is not found in the corresponding index of the secret, the nested while loop will execute and find if the letter is present in any index of the secret.
+    alt_index = 0  # to make sure the next letter is evaluated against every letter of the secret, reset the alt_index variable back to 0.
 
 print(boxes)
 
