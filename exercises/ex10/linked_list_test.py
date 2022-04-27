b646ca492f2ec1c8f0968d8c678d5225109ea7a9
Your Name <you@example.com>
2022-04-27 10:42:03 -0400

    new

--- exercises/ex10/linked_list_test.py
+++ exercises/ex10/linked_list_test.py
@@ -1,7 +1,7 @@
 """Tests for linked list utils."""
 
 import pytest
-from exercises.ex10.linked_list import Node, last, value_at
+from exercises.ex10.linked_list import Node, last, value_at, max, linkify
 
 __author__ = "Your PID"
 
@@ -39,4 +39,23 @@ def test_value_at_out_of_range() -> None:
         value_at(linked_list, 6)
     exception_raised = exc_info.value
     assert str(exception_raised) == "Index is out of bounds on the list."
+
+
+def test_max_base() -> None:
+    """Test to see if the function works with a normal linked list full of values."""
+    linked_list = Node(1, Node(11, Node(15, Node(4, None))))
+    assert max(linked_list) == 15
+
+
+def test_max_empty() -> None:
+    """When the input is None, return a ValueError."""
+    with pytest.raises(ValueError) as exc_info:
+        max(None)
+    exception_raised = exc_info.value
+    assert str(exception_raised) == "Cannot call max with None"
+
+
+def test_linkify_normal() -> None:
+    list = [1, 2, 3, 4]
+    assert linkify(list) == Node(1, Node(2, Node(3, Node(4, None))))
 
